01ad3b9b770287424d6b4c25d8c578dbeb6986e8
Thadisettybharadwaj <thadisettybharadwaj@gmail.com>
2026-02-20 15:43:17 +0530

    chore: renamed health route in server, ref: NOISSUE

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import healthRoute from './api/health';
+import healthRoute from './api/health.route';
 import transferRoutes from './api/transfer.route';
 
 const app = express();
