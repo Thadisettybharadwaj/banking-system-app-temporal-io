a3a50456ce152c55af4149eedd2ca223704948d8
Thadisettybharadwaj <thadisettybharadwaj@gmail.com>
2026-02-20 12:10:44 +0530

    fix: server resource in trasfers, ref: NOISSUE

--- server/index.ts
+++ server/index.ts
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
 app.get('/health', healthRoute);
 
 /** Transfer Routes */
-app.post('/api', transferRoutes);
+app.use('/api', transferRoutes);
 
 /** Server Running */
 app.listen(PORT, () => {
